1717258e38282b321cf67d2eb8a8310ffedb1dc7
6455robloxtv <6455robloxtv@gmail.com>
2025-01-19 19:35:08 +0000

    Update meat.js

--- meat.js
+++ meat.js
@@ -2005,7 +2005,7 @@ if (video.isRestricted || video.isPrivate || video.isDeleted) {
       });
       return;
     }
-    if ((clrurl.match(/cdn.discordapp.com/gi) || clrurl.match(/media.discordapp.net/gi) || clrurl.match(/raw.githubusercontent.com/gi) || clrurl.match(/i.ibb.co/gi)) && (clrurl.match(/.jpeg/gi) || clrurl.match(/.gif/gi) || clrurl.match(/.png/gi) || clrurl.match(/.webp/gi))) {
+    if ((clrurl.match(/cdn.discordapp.com/gi) || clrurl.match(/files.catbox.moe/gi) || clrurl.match(/raw.githubusercontent.com/gi) || clrurl.match(/i.ibb.co/gi)) && (clrurl.match(/.jpeg/gi) || clrurl.match(/.gif/gi) || clrurl.match(/.png/gi) || clrurl.match(/.webp/gi))) {
       this.public.color = "empty";
       this.public.color_cross = clrurl;
       this.room.updateUser(this);
